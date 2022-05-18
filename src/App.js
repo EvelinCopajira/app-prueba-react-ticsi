55e74550653dd39eca4856b26a05b3d31df667b9
evelin copajira <evelincopajira@gmail.com>
2022-05-17 23:10:33 -0300

    navBar con react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+//importo el componente desde la ruta del archivo, para que el import se "prenda" hay que invocarlo como si llamara una etiqueta y lo pinta en verde porque es componente
+
+//import {dateFormat, dateFormat2} from './components/NavBar/NavBar'
 
 function App() {
   return (
     //JSX
     <div className="App">
-      <h1>Ecommerce 31180</h1>
+      <NavBar />
+      Contenido de la app
     </div>
   );
 }
